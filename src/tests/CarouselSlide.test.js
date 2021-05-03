d84eaada9c1aa961d08e67971435b74ebc87768f
rommel667 <rommel667@gmail.com>
2021-05-03 20:44:53 +0800

    :white_check_mark: Add style test for the <img>

--- src/tests/CarouselSlide.test.js
+++ src/tests/CarouselSlide.test.js
@@ -2,6 +2,31 @@ import React from 'react'
 import { shallow, mount } from 'enzyme'
 import CarouselSlide from '../CarouselSlide'
 
+describe('Img', () => {
+    let mounted;
+    const imgUrl = 'https://example.com/default.jpg';
+    
+    beforeEach(() => {
+        const Img = CarouselSlide.defaultProps.Img
+        mounted = mount(<Img src={imgUrl} imgHeight={500} />)
+    })
+
+    it('renders an <img> with the given src', () => {
+        expect(mounted.containsMatchingElement(<img src={imgUrl} />)).toBe(true);
+    })
+
+    it('has the expected static style', () => {
+        expect(mounted).toHaveStyleRule('width', '100%')
+        expect(mounted).toHaveStyleRule('object-fit', 'cover')
+    })
+
+    it('uses imgHeight as the height style property', () => {
+        expect(mounted).toHaveStyleRule('height', '500px')
+        mounted.setProps({ imgHeight: 'calc(100vh - 100px)' });
+        expect(mounted).toHaveStyleRule('height', 'calc(100vh - 100px)');
+    })
+})
+
 describe('CarouselSlide', () => {
     let wrapper;
 
@@ -46,17 +71,5 @@ describe('CarouselSlide', () => {
         expect(wrapper.prop('className')).toBe(className)
     })
 
-    describe('Img', () => {
-        let mounted;
-        const imgUrl = 'https://example.com/default.jpg';
-        
-        beforeEach(() => {
-            const Img = CarouselSlide.defaultProps.Img
-            mounted = mount(<Img src={imgUrl} imgHeight={500} />)
-        })
-
-        it('renders an <img> with the given src', () => {
-            expect(mounted.containsMatchingElement(<img src={imgUrl}/>)).toBe(true)
-        })
-    })
+    
 })
